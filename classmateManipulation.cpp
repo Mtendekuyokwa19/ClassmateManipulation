7ab8d8e81ad783e6e1adc6e8d087064dae47fa8b
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2023-10-01 07:40:51 +0200

    creating a cli prompt

--- classmateManipulation.cpp
+++ classmateManipulation.cpp
@@ -6,7 +6,26 @@ using namespace std;
 int main() {
 
 	classmate newStudent;
-
+	cout<<"--------------------Welcome--------------------\n";
+	cout<<"What operation do you want to run? \n";
+	cout<<"1.Enter new record \n 2.get records with N and M \n";
+	int choice;
+	cin>>choice;
+	
+switch (choice)
+{
+case 1:
+	cout<<"how many records do ou wanna enter"<<endl;
+	int NumberofRecords;
+	cin>>NumberofRecords;
+	collectdata(newStudent,NumberofRecords);
+	break;
+case 2:
+	nameclass();
+	break;
+default:
+	break;
+}
 nameclass();
 
 
