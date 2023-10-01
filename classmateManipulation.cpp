2b679227284b8a99a6960c6776832e30bdab339d
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2023-10-01 07:29:18 +0200

    answering question2

--- classmateManipulation.cpp
+++ classmateManipulation.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "./headerFiles/table.h"
 #include "./headerFiles/collectdata.h"
-
+#include "./headerFiles/collectdata.cpp"
+using namespace std;
 int main() {
 
+	classmate newStudent;
 
+nameclass();
 
 
 	return 0;
--- headerFiles/collectdata.cpp
+++ headerFiles/collectdata.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 void collectdata (classmate newstudent,int rerun){
+
+
     
     ofstream recordbook;
 
@@ -16,23 +18,23 @@ for (int i = 0; i < rerun; i++)
 cout<<"-----------------------New Student--------------------------\n";
 cout<<"1.Enter firstName "<<endl;
 cin>>newstudent.firstName;
-recordbook<<newstudent.firstName;
+recordbook<<newstudent.firstName<<" ";
 
 cout<<"2.Enter Surname "<<endl;
 cin>>newstudent.surname;
-recordbook<<newstudent.surname;
+recordbook<<newstudent.surname<<" ";
 
 cout<<"3.Enter Age "<<endl;
 cin>>newstudent.age;
-recordbook<<newstudent.age;
+recordbook<<newstudent.age<<" ";
 
 cout<<"4.Enter district"<<endl;
 cin>>newstudent.district;
-recordbook<<newstudent.district;
+recordbook<<newstudent.district<<" ";
 
 cout<<"5.Enter Phonenumber "<<endl;
 cin>>newstudent.phoneNumber;
-recordbook<<newstudent.phoneNumber<<endl;
+recordbook<<newstudent.phoneNumber<<" "<<endl;
 
 
 }
@@ -42,3 +44,27 @@ recordbook<<newstudent.phoneNumber<<endl;
 
 recordbook.close();
 }
+
+void nameclass(){
+
+ifstream recordbook;
+recordbook.open("recordbook.txt");
+string line;
+while (!recordbook.eof())
+{
+ getline(recordbook,line);
+   string letter=line.substr(0,1);
+
+    if(letter=="M"|letter=="N"){
+
+
+        cout<<line<<endl;
+    }
+
+}
+
+
+
+}
+
+
